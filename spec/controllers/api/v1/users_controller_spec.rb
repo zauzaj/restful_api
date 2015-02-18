68fd979ef54728009662c0383cd6bbf79de17a4d
zauzaj <jordanovskibojan@gmail.com>
2015-02-18 14:18:53 +0100

    Add update action to users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -51,4 +51,40 @@ describe Api::V1::UsersController do
 			it{ should respond_with 422 }
 		end
 	end
+	
+	describe 'PUT/PATCH #update' do
+	  
+		context 'when is successfully updated' do
+	  	before(:each) do 
+				@user = FactoryGirl.create :user
+				patch :update, {id: @user.id, user: { email: "newemail@bar.com" } }, format: :json
+			end
+			
+			it 'renders the json represantation for the updated user' do
+			  user_response = JSON.parse(response.body, symbolize_names: true)
+				expect(user_response[:email]).to eql "newemail@bar.com"
+			end
+			
+			it { should respond_with 200 }
+	  end
+		
+		context 'when is not created' do
+		  before(:each) do
+				@user = FactoryGirl.create :user
+				patch :update, {id: @user.id, user: {email: "wrongemail.com"} }, format: :json
+			end
+			
+			it 'renders an error json' do
+			  user_response = JSON.parse(response.body, symbolize_names: true)
+				expect(user_response).to have_key(:errors)
+			end
+			
+			it 'renders the json errors on whye the user could not be created' do
+			  user_response = JSON.parse(response.body, symbolize_names: true)
+				expect(user_response[:errors][:email]).to include "is invalid"
+			end
+			
+			it { should respond_with 422 }
+		end
+	end
 end
