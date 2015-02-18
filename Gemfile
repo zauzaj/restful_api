9d800a6f9bab6532e49bfa00d258e722b56cf47c
zauzaj <jordanovskibojan@gmail.com>
2015-02-18 15:46:48 +0100

    Add auth_token to user

--- Gemfile
+++ Gemfile
@@ -38,6 +38,12 @@ group :development, :test do
 end
 
 gem 'devise'
+
+gem 'sabisu_rails', github: "IcaliaLabs/sabisu-rails"
+gem 'compass-rails'
+gem 'furatto'
+gem 'font-awesome-rails'
+gem 'simple_form'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211162648) do
+ActiveRecord::Schema.define(version: 20150218134334) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150211162648) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "auth_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
